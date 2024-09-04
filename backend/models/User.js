9c39442731788f289299095cfac75e44e1c923b8
Michael Vasilevsky <michael@dikuw.com>
2024-09-04 17:47:32 -0400

    initial commit

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+// // start from Hely, maybe want to add this in the future
+// const validator = require('validator');
+// const mongodbErrorHandler = require('mongoose-mongodb-errors');
+// const passportLocalMongoose = require('passport-local-mongoose');
+// // end from Hely
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+    required: 'Please enter an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please enter a name',
+    trim: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+// // start from Hely, maybe want to add this in the future
+// userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+// userSchema.plugin(mongodbErrorHandler);
+// // end from Hely
