b1d53de4ef54469f0fa5a12a8b14423259a5ebf5
Michael Vasilevsky <michael@dikuw.com>
2024-09-04 18:12:50 -0400

    added addDocument route

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import { connectDB } from './database/index.js';
 
+import { Document } from './models/Document';
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("This is the WI Writer backend/API.");
 });
+
+app.post("/addDocument", async (req, res) => {
+  const document = req.body;
+  const newDocument = new Document(document);
+  try {
+    await newDocument.save();
+    res.status(201).json({ success: true, data: newDocument });
+  } catch(error) {
+    console.error("Error in add document:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+})
 
 app.listen(5000, () => {
   connectDB();
