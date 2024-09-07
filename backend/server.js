5bbb4aa2f8743cb33671d9a3b8354ea9af61f5c6
Michael Vasilevsky <michael@dikuw.com>
2024-09-07 08:47:17 -0400

    add getDocuments endpoint

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,22 @@ app.get("/", (req, res) => {
   res.send("This is the WI Writer backend/API.");
 });
 
+app.get("/getDocuments", async (req, res) => {
+  try {
+    const documents = await Document.find();
+    if (!documents.length) {
+      return res
+        .status(404)
+        .json({ success: false, error: `No documents found` })
+    }
+    res.status(200).json({ success: true, data: documents });
+  } catch(error) {
+    console.error("Error in add document:", error.message);
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+
 app.post("/addDocument", async (req, res) => {
   const document = req.body;
   const newDocument = new Document(document);
@@ -19,9 +35,9 @@ app.post("/addDocument", async (req, res) => {
     res.status(201).json({ success: true, data: newDocument });
   } catch(error) {
     console.error("Error in add document:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
+    res.status(500).json({ success: false, message: error });
   }
-})
+});
 
 app.listen(5000, () => {
   connectDB();
