458a6e337339cca93ca88d0e278c51d03fb9235c
Michael Vasilevsky <michael@dikuw.com>
2024-09-09 07:15:04 -0400

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Homepage />} />  */}
+    </Routes>
+  )
+}
+
+export default App
