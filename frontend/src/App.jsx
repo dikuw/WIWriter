668261344a698cae76934b5b22b1ac3b2af9e5d1
Michael Vasilevsky <michael@dikuw.com>
2024-09-09 19:59:39 -0400

    add Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 
+import TopBanner from './components/header/TopBanner';
+import Header from './components/header/Header';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isPasswordIncorrect, setIsPasswordIncorrect] = useState(false);
 
   return (
-    <Routes>
-      {/* <Route path="/" element={<Homepage />} />  */}
-    </Routes>
-  )
+    <div>
+      <TopBanner isLoggedIn={isLoggedIn} name={user.name}/>
+      <Header /> 
+      <Routes>
+        
+        {/* <Route path="/" element={<Homepage />} />  */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App
