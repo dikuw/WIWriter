d0a56823e4322283de0387ebdba7b059aa9a238b
Michael Vasilevsky <michael@dikuw.com>
2024-09-10 19:57:55 -0400

    add admin route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react'
 
 import TopBanner from './components/header/TopBanner';
 import Header from './components/header/Header';
+import Banner from './components/header/Banner';
+import Navigation from './components/navigation/Navigation';
+import Admin from './components/admin/Admin';
 
-function App() {
+function App(props) {
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -13,13 +16,31 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isPasswordIncorrect, setIsPasswordIncorrect] = useState(false);
 
+  const logoutUser = async (user) => {
+    const payload = { ...user };
+    await apis.logout(payload).then(res => {
+      setIsLoggedIn(false);
+      setUserOrders([]);
+      setOrders([]);
+      setCart([]);
+      props.history.push("/");
+    });
+  }
+
   return (
     <div>
       <TopBanner isLoggedIn={isLoggedIn} name={user.name}/>
       <Header /> 
       <Routes>
-        
-        {/* <Route path="/" element={<Homepage />} />  */}
+        <Route path="/admin" 
+          element={
+            <>
+              <Banner bannerString={"Site Administration"} />
+              <Navigation isLoggedIn={isLoggedIn} isAdmin={user.isAdmin} history={props.history} logoutUser={logoutUser} />
+              <Admin history={props.history} />
+            </>
+          }
+        />
       </Routes>
     </div>
   );
