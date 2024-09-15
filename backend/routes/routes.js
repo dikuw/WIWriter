ea914eb78e2bd740dd5e4231100c99e5b97c5c62
Michael Vasilevsky <michael@dikuw.com>
2024-09-15 10:45:56 -0400

    add .js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userController } from '../controllers/userController';
+import { userController } from '../controllers/userController.js';
 
 // // start from Hely, maybe want to add this in the future
 // const passport = require('passport');
